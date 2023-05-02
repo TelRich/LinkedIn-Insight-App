dfd49b6659b400e60a28be7d10c84dbd736559b2
TelRich <okorogoodrich@gmail.com>
2023-05-02 11:47:26 +0100

    implemented try and except

--- pages/LinkedIn_Invitations.py
+++ pages/LinkedIn_Invitations.py
@@ -13,11 +13,14 @@ import plotly.express as px
 st.markdown("<h3 style='text-align:center;'>LinkedIn Invitations</h3>", unsafe_allow_html=True)
 file = st.file_uploader('Upload Invitations', type="csv")
 
-@st.cache_data
-def save_upload():
-    uploaded_file = pd.read_csv(file)
-    return uploaded_file
-df = save_upload()
+try:
+    @st.cache_data
+    def save_upload():
+        uploaded_file = pd.read_csv(file, skiprows=[0,1])
+        return uploaded_file
+    df = save_upload()
 
-if df is not None:
-    st.write(df)
+    if df is not None:
+        st.write(df)ss
+except ValueError:
+    print('Upload your LinkedIn CSV file')
